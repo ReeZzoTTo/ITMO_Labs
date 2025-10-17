2d44efdb35461f14894ceb42ce5583f3d4e11904
ReeZzoTTo <diomand386@gmail.com>
2025-10-17 22:52:56 +0300

    Update Main.java

--- Lab2Prog/src/Main.java
+++ Lab2Prog/src/Main.java
@@ -29,19 +29,3 @@ public class Main {
         battle.go();
     }
 }
-
-
-
-
-// scp -P 2222 Pokemon.jar s501178@se.ifmo.ru:~/lab2prog
-// scp -P 2222 Main.java s501178@se.ifmo.ru:~/lab2prog
-// scp -r -P 2222 pokemonAttacks s501178@se.ifmo.ru:~/lab2prog
-// scp -r -P 2222 pokemonClasses s501178@se.ifmo.ru:~/lab2prog
-
-// ssh -p 2222 s501178@se.ifmo.ru
-// cd lab2prog
-// javac -cp .:Pokemon.jar Main.java pokemonAttacks/*.java pokemonClasses/*.java
-// touch manifest.txt
-// echo "Main-Class: Main" >> manifest.txt
-// echo "Class-Path: Pokemon.jar" >> manifest.txt
-// jar cfm MyPokemonBattle.jar manifest.txt Main.class pokemonAttacks/*.class pokemonClasses/*.class
